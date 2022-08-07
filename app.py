9b83251c7e7308f1d850dacf9fd069d4918e4989
Subodh <subodhsinghrathore70@gmail.com>
2022-08-07 14:09:40 +0530

    Added feature/search method,create_playlist method, Added more skeletons and Elements into Albums Arijit Singh and Black Panther.

--- app.py
+++ app.py
@@ -147,6 +147,143 @@ def is_logged_in(f):
 			return redirect(url_for('login'))
 	return wrap
 
+
+#search
+@app.route('/new',methods=['POST'])
+def new():
+	string=""
+	co=request.form['give']
+	song=co
+	song_name=co+'.mp3'
+	cur=mysql.connection.cursor()
+	result=cur.execute("SELECT * FROM songs_list WHERE song_name=%s",[song_name])
+	albu69=cur.fetchall()
+	if result>0:
+		return render_template('search.html',albu=albu69)
+	else:
+		try:
+			page = requests.get("https://www.youtube.com/results?search_query="+song)
+			soup = BeautifulSoup(page.text,'html.parser')
+			for div in soup.find_all('div', { "class" : "yt-lockup-video" }):
+				if div.get("data-context-item-id") != None:
+					video_id = div.get("data-context-item-id")
+					break
+			os.system('youtube-dl --extract-audio --audio-format mp3 -o "akhil.mp3" https://www.youtube.com/watch?v='+video_id)
+			os.system("mv *.mp3 ./static/music/")
+			os.rename("static/music/akhil.mp3","static/music/"+song_name)
+			string="/static/music/"+song_name
+			cur=mysql.connection.cursor()
+			cur.execute("INSERT INTO songs_list(path,album,song_name) VALUES (%s,%s,%s)",(string,"NA",song_name))
+			mysql.connection.commit()
+			result=cur.execute("SELECT * FROM songs_list WHERE song_name=%s",[song_name])
+			albu99=cur.fetchall()
+			return render_template('search.html',albu=albu99)
+		except NameError:
+			flash('Song Not Found','success')
+			return render_template('dashboard.html')
+
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+	cur=mysql.connection.cursor()
+
+	result=cur.execute("SELECT * from songs WHERE user_id = %s",[session['id']])
+
+	songs=cur.fetchall()
+
+	if result>0:
+		return render_template('dashboard.html',songs=songs)
+	else:
+		msg="NO PLAYLIST FOUND "
+
+	return render_template('dashboard.html',msg=msg)
+	cur.close()
+
+class make_playlist(Form):
+	title=StringField('Name',[validators.Length(min=1,max=25)])
+
+@app.route('/create_playlist',methods=['GET','POST'])
+@is_logged_in
+def create_playlist():
+	form=make_playlist(request.form)
+	if request.method=='POST' and form.validate():
+		title=form.title.data
+		cur=mysql.connection.cursor()
+		username=session['username']
+		row=cur.execute("SELECT * FROM users WHERE username = %s",[username])
+		result=cur.fetchone()
+		idd=result['id']
+		cur.execute("INSERT INTO songs(title,user_id) VALUES (%s,%s)",([title],idd,))
+		cur.execute("UPDATE songs SET type=1 WHERE title=%s",([title]))
+		mysql.connection.commit()
+		cur.close()
+
+		flash(idd,'success')
+
+		return redirect(url_for('dashboard'))
+	return render_template('add_playlist.html',form=form)
+
+@app.route('/users')
+@is_logged_in
+def users():
+	cur=mysql.connection.cursor()
+	result=cur.execute("SELECT * from users")
+	songs=cur.fetchall()
+	if result>0:
+		return render_template('Dashboard.html',songs=songs)
+	else:
+		msg="NO PLAYLIST FOUND "
+
+	return render_template('Dashboard.html',msg=msg)
+	cur.close()
+
+@app.route('/users/playlist/<string:idd>')
+@is_logged_in
+def u_play(idd):
+	cur=mysql.connection.cursor()
+	result=cur.execute("SELECT * from songs WHERE user_id=%s and type=1",[idd])
+	songs=cur.fetchall()
+	if result>0:
+		return render_template('das.html',songs=songs)
+	else:
+		msg="NO PLAYLIST FOUND "
+	return render_template('das.html',msg=msg)
+	cur.close()
+
+@app.route('/Arijit Singh')
+@is_logged_in
+def play():
+	cur=mysql.connection.cursor()
+	cur.execute("SELECT * FROM songs_list WHERE album LIKE 'rep%'")
+	albu=cur.fetchall()
+	result=cur.execute("SELECT * from songs WHERE user_id = %s",[session['id']])
+	songs=cur.fetchall()
+	if result>0:
+		return render_template('home.html',songs=songs,albu=albu)
+	else:
+		songs=0
+		return render_template('home.html',albu=albu,song=songs)
+	cur.close()
+	return render_template('home.html',albu=albu)
+    
+@app.route('/BlackPanther')
+@is_logged_in
+def panther():
+			cur=mysql.connection.cursor()
+			cur.execute("SELECT * FROM songs_list WHERE album LIKE 'bla%'")
+			albu3=cur.fetchall()
+			result=cur.execute("SELECT * from songs WHERE user_id = %s",[session['id']])
+			songs=cur.fetchall()
+			if result>0:
+				return render_template('blackpanther.html',songs=songs,albu=albu3)
+			else:
+				songs=0
+				return render_template('blackpanther.html',albu=albu3,song=songs)
+			cur.close()
+			#    app.logger.info(albu[11]["path"]
+			return render_template('blackpanther.html',albu=albu3)
+
 if __name__=='__main__':
     app.secret_key='secret123'
     app.run(debug=True)
